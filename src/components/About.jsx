15a8357161200d2359d7f3ae92cebc99d70160c4
Abdul Saboor <abdulsaboor.17.4.2004@gmail.com>
2024-03-24 04:26:15 +0500

    about section is responsive now!

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import { IoGameController } from "react-icons/io5";
 import { MdArrowOutward } from 'react-icons/md';
+import { HiGlobeAlt } from "react-icons/hi";
+import { FaPenNib } from "react-icons/fa";
+import { HiSpeakerphone } from "react-icons/hi";
 
 const About = () => (
   <article className='w-full font-["NeueMontreal"] font-medium text-dark bg-[#CDEA68] rounded-tl-3xl rounded-tr-3xl -mt-5'>
@@ -24,29 +28,29 @@ const About = () => (
       </a>{' '}
       just for your success.
     </h2>
-    <div className='w-full py-10 border mt-10 border-t-dark/20 border-b-dark/20 flex '>
-      <div className='w-1/2 pl-[2vw]'>
-        <h1 className='text-[3vmax] font-bold'>Our Best Services</h1>
-        <ul className='text-2xl pl-[2vw] my-5 list-disc '>
-          <a>
-            <li>Game Development</li>
+    <div className='w-full py-10 border mt-10 border-t-dark/20 border-b-dark/20 flex'>
+      <div className='w-[100vw] md:w-1/2 pl-[2vw] flex flex-col '>
+        <h1 className='text-center md:text-[3.7vmax] text-[4.9vmax]  font-bold'>Our Best Services</h1>
+        <ul className='w-full md:text-[2.3vmax] text-[3.3vmax]  my-5 flex flex-col mt-14'>
+          <a className='hover:animate-pulse flex' >
+            <li className='flex items-center gap-5'><IoGameController className='border border-dark rounded-full p-1 '/> Game Development </li>
           </a>
-          <a>
-            <li>Website Development</li>
+          <a className='hover:animate-pulse flex' >
+            <li className='flex items-center gap-5'><HiGlobeAlt className='border border-dark rounded-full p-1 '/> Website Development </li>
           </a>
-          <a>
-            <li>Graphics Design</li>
+          <a className='hover:animate-pulse flex' >
+            <li className='flex items-center gap-5'><FaPenNib className='border border-dark rounded-full p-1 '/> Graphics Design </li>
           </a>
-          <a>
-            <li>Digital Marketing</li>
+          <a className='hover:animate-pulse flex' >
+            <li className='flex items-center gap-5'><HiSpeakerphone className='border border-dark rounded-full p-1 '/> Digital Marketing </li>
           </a>
         </ul>
-        <button className='py-3 px-5 text-dim_white my-6 outline-none bg-dark rounded-full leading-none uppercase flex gap-2 items-center justify-around '>
-          I want to Know More
-          <MdArrowOutward className='' />
+        <button className='text-center w-1/2 py-3 px-4 text-dim_white my-5 outline-none bg-dark rounded-full leading-none uppercase flex md:gap-2 items-center justify-around '>
+          Start My Project
+          <MdArrowOutward  />
         </button>
       </div>
-      <div className='w-1/2 h-[59vh] overflow-hidden drop-shadow-2xl rounded-2xl bg-dark/20 mr-[2vw]'>
+      <div className='hidden md:block w-1/2 h-[59vh] overflow-hidden drop-shadow-2xl rounded-2xl bg-dark/20 mr-[2vw]'>
         <div className="w-full h-full bg-cover bg-center bg-[url('./assets/images/developing_closeup.webp')]"></div>
       </div>
     </div>
